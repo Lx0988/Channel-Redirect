afa24dd8e1a48ce88db881c0ba750762a58770fd
LX <93909675+0Lx0@users.noreply.github.com>
2022-12-10 22:32:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @trojanzhex
-
-
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from script import script
