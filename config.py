1e84681641e95fe0a761d300ecdd67c4ca8210bc
LX <93909675+0Lx0@users.noreply.github.com>
2022-12-10 20:42:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ from logging.handlers import RotatingFileHandler
 
 
 # Get a bot token from botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5300015911:AAHiwT8Ze-8bR3PaN0xGKlzxqNWADRFl7jg")
 
 # Get from my.telegram.org (or @UseTGXBot)
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "7961171"))
 
 # Get from my.telegram.org (or @UseTGXBot)
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "b2d798626afaf5f1e4bdb33988e3d377")
 
 # Generate a user session string 
-TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQBFcTPHCdz1KIUDTBghJjJxoop7sXzdQ23pynzKHbmXB9yEKGPGhMpZ9m3JVrJVIaOHpK3rJwPKVTzdJ1lS3uEm_01ecfeY0hAnfLhNWo433D-iA-LofkjogqnfBAxTbVv81GSkDfpJbDgV3Ew-fx_WZYvZD5ria5_niXm7KREGWaQoXbTOqiHquhSv0tqo5_2Gv300rOqA9X2BQxiTB_qIQV-EvSXw0Wn33Ou7L6vcXplsyj52tLqfiybzELl87eaHZrTtKfHiJR52WZetNs8xfOX10muOQYuPKUek_u0JvwcERuGv5_mY1Hj6GJS4aN2eMdo_X1A56e3EcpEspIkCAAAAAUSZaVUA")
 
 # ID of Channel from which the bot shoul search files
-MAINCHANNEL_ID = os.environ.get("MAINCHANNEL_ID", "")
+MAINCHANNEL_ID = os.environ.get("MAINCHANNEL_ID", "-1001531149575")
 
 
 
